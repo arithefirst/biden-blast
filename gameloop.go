9a38d448de7663a4a310944a6a1d00e0102efe49
April Hall <ari@arithefirst.com>
2024-11-14 12:36:19 -0500

    Move the game loop to gameloop.go

---
+++ gameloop.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+// Update runs every tick/frame
+func (g *Game) Update() error {
+	// Get the player's size
+	playerWidth := g.player.Image.Bounds().Dx()
+	playerHeight := g.player.Image.Bounds().Dy()
+
+	// Move Left
+	if (ebiten.IsKeyPressed(ebiten.KeyLeft) || ebiten.IsKeyPressed(ebiten.KeyA)) &&
+		g.player.XPos > 0 {
+		g.player.XPos -= 2 * moveSpeed
+		fmt.Printf("Player Pos: %v %v\n", g.player.XPos, g.player.YPos)
+	}
+
+	// Move Right
+	if (ebiten.IsKeyPressed(ebiten.KeyRight) || ebiten.IsKeyPressed(ebiten.KeyD)) &&
+		g.player.XPos < float64(GscreenWidth-playerWidth) {
+		g.player.XPos += 2 * moveSpeed
+		fmt.Printf("Player Pos: %v %v\n", g.player.XPos, g.player.YPos)
+	}
+
+	// Move Up
+	if (ebiten.IsKeyPressed(ebiten.KeyUp) || ebiten.IsKeyPressed(ebiten.KeyW)) &&
+		g.player.YPos > 0 {
+		g.player.YPos -= 2 * moveSpeed
+		fmt.Printf("Player Pos: %v %v\n", g.player.XPos, g.player.YPos)
+	}
+
+	// Move Down
+	if (ebiten.IsKeyPressed(ebiten.KeyDown) || ebiten.IsKeyPressed(ebiten.KeyS)) &&
+		g.player.YPos < float64(GscreenHeight-playerHeight) {
+		g.player.YPos += 2 * moveSpeed
+		fmt.Printf("Player Pos: %v %v\n", g.player.XPos, g.player.YPos)
+	}
+
+	return nil
+}
